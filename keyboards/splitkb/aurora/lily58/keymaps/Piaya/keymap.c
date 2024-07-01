cb1ffd4c27858ef538032658c022ea673b81a900
Saúl Gámez <sa.gamez@outlook.com>
2024-07-01 19:10:05 +0200

    change position of include header

--- keyboards/splitkb/aurora/lily58/keymaps/Piaya/keymap.c
+++ keyboards/splitkb/aurora/lily58/keymaps/Piaya/keymap.c
@@ -1,5 +1,9 @@
 #include QMK_KEYBOARD_H
 
+// eneable international characters 
+#include "keymap_us_international.h"
+#include "sendstring_us_international.h"
+
 // turn off led from liatris microcontroller
 void keyboard_pre_init_user(void) {
   // Set our LED pin as output
@@ -9,9 +13,7 @@ void keyboard_pre_init_user(void) {
   writePinHigh(24);
 }
 
-// eneable international characters 
-#include "keymap_us_international.h"
-#include "sendstring_us_international.h"
+
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT(KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_GRV, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_MINS, KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, TO(4), KC_RBRC, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_LALT, KC_LGUI, MO(1), KC_SPC, KC_ENT, MO(2), KC_BSPC, MT(MO(5), KC_DEL)),
